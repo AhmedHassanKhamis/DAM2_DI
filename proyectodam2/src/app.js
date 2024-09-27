89d9d26dc264f0759bceb44ef209d59c91525a99
usertar <ahmedmario@hotmail.es>
2024-09-27 19:29:51 +0200

    nuevas actualizaciones

--- proyectodam2/src/app.js
+++ proyectodam2/src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import morgan from "morgan";
-
+import authRoutes from "./routes/Auth.routes.js"
 
 
 const app = express();
 
 
 app.use(morgan("dev"));
+// app.use(authRoutes);
 
 
 export default app;
---
+++ proyectodam2/src/controllers/Auth.controllers.js
@@ -0,0 +1,2 @@
+export const register = (req, res) => {res.send("registrando")};
+export const login = (req, res) => {res.send("logeandose")};
