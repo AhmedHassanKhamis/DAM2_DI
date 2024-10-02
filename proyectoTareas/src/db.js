170a6529cf38a03b0b8ccd21b4b8a61c839e752e
usertar <ahmedmario@hotmail.es>
2024-10-02 21:09:07 +0200

    ni idea

--- proyectoTareas/src/db.js
+++ proyectoTareas/src/db.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 const  conexiondb = async () =>{
     try {
         await mongoose.connect("mongodb://localhost/Tasks2DAM");
-        console.log("#################### BBDD OK ####################")
+        console.log("#################### BBDD OK ####################");
+        
     } catch (error) {
         
     }
