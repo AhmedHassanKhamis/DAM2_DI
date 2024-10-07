f6ad576121f277737aec8bcb8e43a383b23c4416
usertar <ahmedmario@hotmail.es>
2024-10-07 20:20:36 +0200

    algunas acciones sobre base de datos

--- proyectodam2/src/controllers/auth.controllers.js
+++ proyectodam2/src/controllers/auth.controllers.js
@@ -31,4 +31,46 @@ export const register = async(req, res) => {
 
 
 };
-export const login = (req, res) => {res.send("logeandose")};
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const usuario = await User.findOne({username : username});
+        if (usuario) {
+            if (await bcrypt.compare(password, usuario.password)) {
+                res.send(`Usuario logeado \n -> ${usuario.username}`);
+            }
+        } else {
+            res.send("Usuario no Encontrado");
+        }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+};
+
+
+export const actualizarUsuario = async (req, res) => {
+    const {  username, oldPassword , newPassword } = req.body;
+    try {
+        const usuarioExiste = await User.findOne({username : username});
+        
+        if (usuarioExiste) {
+            if (await bcrypt.compare(oldPassword, usuarioExiste.password)) {
+                const passwordHash = bcrypt.hash..
+                usuarioExiste.password = newPassword;
+
+            }
+        }else{
+            res.send("usuario no encontrado");
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
