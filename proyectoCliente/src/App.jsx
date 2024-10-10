2598588144def54b4dc1d7620c8a9a98083e489d
ahmedhassank <ahmedmario@hotmail.es>
2024-10-10 23:48:52 +0200

    listo el login pocho con tailwind

--- proyectoCliente/src/App.jsx
+++ proyectoCliente/src/App.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <form action="#" method="post" class="w-fit flex flex-col ">
-        <h1 class="">Bienvendio</h1>
+    <div class="flex flex-col h-screen justify-center  drop-shadow">
+      <form action="#" method="post" class="border-solid border-2 border-black p-5 w-fit self-center flex flex-col gap-1 -translate-y-1/2		">
+        <h1 class="self-center text-xl ">Bienvendio</h1>
 
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-
+        <input type="email" name="email" id="email" class="border-solid border-2"/>
 
         <label htmlFor="contrasenia">Contraseña</label>
-        <input type="password" name="contrasenia" id="contrasenia" />
+        <input type="password" name="contrasenia" id="contrasenia" class="border-solid border-2"/>
 
-
-        <input type="submit" value="Enviar" name='Enviar' />
+        <input type="submit" value="Acceder" name='Acceder' class="border-solid border-2 border-black mt-3 bg-blue-300  hover:bg-green-300 ease-in-out duration-700"/>
       </form>
       
     </div>
